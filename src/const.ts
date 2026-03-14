1d2344119352809f60e94e537d87253f116e93dd
amd9878 <amd9878@gmail.com>
2026-03-14 17:19:47 +0900

    청첩장 완성

--- src/const.ts
+++ src/const.ts
@@ -9,15 +9,15 @@ dayjs.locale("ko")
 
 export { dayjs }
 
-export const WEDDING_DATE = dayjs.tz("2024-08-24 13:00", "Asia/Seoul")
+export const WEDDING_DATE = dayjs.tz("2026-08-24 13:00", "Asia/Seoul")
 export const WEDDING_DATE_FORMAT = `YYYY년 MMMM D일 dddd A h시${WEDDING_DATE.minute() === 0 ? "" : " m분"}`
 
 // 예식 당월 휴무일. 켈린더에 표시하기 위함.
 // 예: 예식일 8월 -> 8월 15일 광복절
 export const HOLIDAYS = [15]
 
-export const LOCATION = "서울대학교 연구공원 웨딩홀"
-export const LOCATION_ADDRESS = "서울시 관악구 관악로 1, 연구공원 본관 1층"
+export const LOCATION = "현대자동차 본사 로비"
+export const LOCATION_ADDRESS = "서울시 서초구 헌릉로 12 현대자동차 본사 8층 로비 -"
 
 // 카카오톡 공유 시 위치 정보로 사용할 주소.
 // LOCATION 과 동일하게 설정해도 무방하나, 필요에 따라 좀 더 상세히 작성 가능.
@@ -30,23 +30,23 @@ export const WEDDING_HALL_POSITION = [126.9594982, 37.4657134]
 // 네이버 지도의 웨딩홀 장소 ID
 // 네이버 지도 웹페이지에서 웨딩홀 검색 후 URL에서 확인 가능.
 // 예: https://map.naver.com/p/entry/place/13321741 -> 13321741
-export const NMAP_PLACE_ID = 13321741
+export const NMAP_PLACE_ID = 12104862
 
 // 카카오 지도의 웨딩홀 장소 ID
 // 카카오 지도 웹페이지에서 웨딩홀 검색 후 해당 장소에서 상세보기 클릭 시 URL에서 확인 가능.
 // 예: https://place.map.kakao.com/8634826 -> 8634826
-export const KMAP_PLACE_ID = 8634826
+export const KMAP_PLACE_ID = 8622892
 
-export const BRIDE_FULLNAME = "정지원"
-export const BRIDE_FIRSTNAME = "지원"
+export const BRIDE_FULLNAME = "신태정"
+export const BRIDE_FIRSTNAME = "태정"
 export const BRIDE_TITLE = "장녀"
-export const BRIDE_FATHER = "정상원"
-export const BRIDE_MOTHER = "박윤정"
+export const BRIDE_FATHER = "---"
+export const BRIDE_MOTHER = "---"
 export const BRIDE_INFO = [
   {
     relation: "신부",
     name: BRIDE_FULLNAME,
-    phone: "010-0000-0000",
+    phone: "010-5138-5510",
     account: "우리은행 0000000000000",
   },
   {
@@ -63,17 +63,17 @@ export const BRIDE_INFO = [
   },
 ]
 
-export const GROOM_FULLNAME = "남주호"
-export const GROOM_FIRSTNAME = "주호"
-export const GROOM_TITLE = "차남"
-export const GROOM_FATHER = "남현태"
-export const GROOM_MOTHER = "김현영"
+export const GROOM_FULLNAME = "박성준"
+export const GROOM_FIRSTNAME = "성준"
+export const GROOM_TITLE = "막둥이"
+export const GROOM_FATHER = "박덕유"
+export const GROOM_MOTHER = "최복희"
 export const GROOM_INFO = [
   {
     relation: "신랑",
     name: GROOM_FULLNAME,
-    phone: "010-0000-0000",
-    account: "하나은행 00000000000000",
+    phone: "010-5138-5510",
+    account: "하나은행 121-910486-66207",
   },
   {
     relation: "신랑 아버지",
