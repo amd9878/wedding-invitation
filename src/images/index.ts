a8b7bafc47942c891e3f3af204a7cc258afba3c5
amd9878 <amd9878@gmail.com>
2026-03-15 17:54:18 +0900

    이미지주소 찐막

--- src/images/index.ts
+++ src/images/index.ts
@@ -2,7 +2,7 @@ export const COVER_IMAGE = "https://imgur.com/cdUJPML"
 
 export const GALLERY_IMAGES = [
   "[img]https://i.imgur.com/cdUJPML.png[/img]",
-  "[img]https://i.imgur.com/cdUJPML.png[/img]",
+  "https://i.imgur.com/cdUJPML.png",
   "https://imgur.com/cdUJPML",
   "https://imgur.com/cdUJPML",
   "https://imgur.com/cdUJPML",
