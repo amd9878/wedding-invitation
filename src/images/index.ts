eb45db27d17f44af03c3612365e3c3fed0054367
amd9878 <amd9878@gmail.com>
2026-03-15 17:57:29 +0900

    커버이미지 주소 링크로

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,7 +1,7 @@
-export const COVER_IMAGE = "https://imgur.com/cdUJPML"
+export const COVER_IMAGE = "https://i.imgur.com/cdUJPML.png"
 
 export const GALLERY_IMAGES = [
-  "[img]https://i.imgur.com/cdUJPML.png[/img]",
+  "https://i.imgur.com/cdUJPML.png",
   "https://i.imgur.com/cdUJPML.png",
   "https://imgur.com/cdUJPML",
   "https://imgur.com/cdUJPML",
