95a5c66d28905fe470dbfa1bba81166868661a46
amd9878 <amd9878@gmail.com>
2026-03-15 17:52:44 +0900

    imgur 주소 변경

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,8 +1,8 @@
 export const COVER_IMAGE = "https://imgur.com/cdUJPML"
 
 export const GALLERY_IMAGES = [
-  "https://imgur.com/cdUJPML",
-  "https://imgur.com/cdUJPML",
+  "[img]https://i.imgur.com/cdUJPML.png[/img]",
+  "[img]https://i.imgur.com/cdUJPML.png[/img]",
   "https://imgur.com/cdUJPML",
   "https://imgur.com/cdUJPML",
   "https://imgur.com/cdUJPML",
