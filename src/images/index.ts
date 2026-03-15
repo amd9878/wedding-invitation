1326172cae1106269826e52797b0a8dbaa50871b
amd9878 <amd9878@gmail.com>
2026-03-15 17:42:08 +0900

    이미지 링크 업로드

---
+++ src/images/index.ts
@@ -0,0 +1,16 @@
+export const COVER_IMAGE = "https://imgur.com/a/ELGysbs"
+
+export const GALLERY_IMAGES = [
+  "https://imgur.com/a/ELGysbs",
+  "https://imgur.com/a/ELGysbs",
+  "https://imgur.com/a/ELGysbs",
+  "https://imgur.com/a/ELGysbs",
+  "https://imgur.com/a/ELGysbs",
+  "https://imgur.com/a/ELGysbs",
+  "https://imgur.com/a/ELGysbs",
+  "https://imgur.com/a/ELGysbs",
+  "https://imgur.com/a/ELGysbs",
+  "https://imgur.com/a/ELGysbs",
+  "https://imgur.com/a/ELGysbs",
+  "https://imgur.com/a/ELGysbs",
+]
