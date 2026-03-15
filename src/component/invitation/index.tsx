d77a4875cabd2789a8ce8afd62e0eb87c19c31ab
amd9878 <amd9878@gmail.com>
2026-03-15 18:03:27 +0900

    문구 수정

--- src/component/invitation/index.tsx
+++ src/component/invitation/index.tsx
@@ -25,17 +25,13 @@ export const Invitation = () => {
 
       <div className="break" />
 
-      <div className="content">싱그러운 여름 향기 가득한 날</div>
-      <div className="content">소중한 분들을 모시고</div>
-      <div className="content">사랑의 약속을 하려고 합니다.</div>
+      <div className="content">"우리는 모두 조금씩 다르지만,</div>
+      <div className="content">그래서 서로에게 더 특별합니다."</div>
       <div className="break" />
-      <div className="content">햇살이 뜨거울 땐 가려주고,</div>
-      <div className="content">비가 오면 우산이 되어주는</div>
-      <div className="content">부부가 되겠습니다.</div>
+      <div className="content">여우와 토끼처럼 다른 두 사람이 만나</div>
+      <div className="content">평생의 팀이 되기로 해</div>
       <div className="break" />
-      <div className="content">기쁜날 함께 하시어</div>
-      <div className="content">저희의 앞날을 축복해 주세요.</div>
-
+      <div className="content">저희의 첫 페이지를 함께해 주세요.</div>
       <div className="break" />
 
       <div className="name">
