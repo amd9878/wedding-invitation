d9371245a385ac1b556be4425e6f6e06b00f2dfe
amd9878 <amd9878@gmail.com>
2026-03-15 17:49:24 +0900

    imgur 주소 에러 수정

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,16 +1,16 @@
-export const COVER_IMAGE = "https://imgur.com/a/ELGysbs"
+export const COVER_IMAGE = "https://imgur.com/cdUJPML"
 
 export const GALLERY_IMAGES = [
-  "https://imgur.com/a/ELGysbs",
-  "https://imgur.com/a/ELGysbs",
-  "https://imgur.com/a/ELGysbs",
-  "https://imgur.com/a/ELGysbs",
-  "https://imgur.com/a/ELGysbs",
-  "https://imgur.com/a/ELGysbs",
-  "https://imgur.com/a/ELGysbs",
-  "https://imgur.com/a/ELGysbs",
-  "https://imgur.com/a/ELGysbs",
-  "https://imgur.com/a/ELGysbs",
-  "https://imgur.com/a/ELGysbs",
-  "https://imgur.com/a/ELGysbs",
+  "https://imgur.com/cdUJPML",
+  "https://imgur.com/cdUJPML",
+  "https://imgur.com/cdUJPML",
+  "https://imgur.com/cdUJPML",
+  "https://imgur.com/cdUJPML",
+  "https://imgur.com/cdUJPML",
+  "https://imgur.com/cdUJPML",
+  "https://imgur.com/cdUJPML",
+  "https://imgur.com/cdUJPML",
+  "https://imgur.com/cdUJPML",
+  "https://imgur.com/cdUJPML",
+  "https://imgur.com/cdUJPML",
 ]
